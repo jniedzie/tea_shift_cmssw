08ffacbec3d59b679e7350e872ea35abe01d49c7
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2026-08-27 14:53:01 +0200

    Fix campaign name formatting in shift_paths.py for trigger proxy analysis

--- configs/shift_paths.py
+++ configs/shift_paths.py
@@ -9,5 +9,4 @@ sample = "jpsi"
 # campaign = "Charmonium_FixedTarget_pThat_1to5GeV_13p6TeV_100k_beamB"
 # campaign = "Charmonium_FixedTarget_pThat_1to5GeV_13p6TeV_timing_2023"
 # campaign = "Charmonium_FixedTarget_pThat_1to5GeV_13p6TeV_occupancy_2023"
-campaign = "Charmonium_FixedTarget_pThat_1to5GeV_13p6TeV__triggerProxy_2023"
-
+campaign = "Charmonium_FixedTarget_pThat_1to5GeV_13p6TeV_triggerProxy_2023"
